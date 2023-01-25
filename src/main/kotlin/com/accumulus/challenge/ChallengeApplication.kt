13cb4ad305e75bbfee0f2e082af1da26325190e9
ate <ate@ates-MacBook-Pro.local>
2023-01-25 08:28:29 -0500

    Making progress

--- src/main/kotlin/com/accumulus/challenge/ChallengeApplication.kt
+++ src/main/kotlin/com/accumulus/challenge/ChallengeApplication.kt
@@ -1,14 +1,33 @@
 package com.accumulus.challenge
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
-// data class PersonToppingsChoice(val name: String, val topics: List<String>)
+data class UserToppingsSubmission(val email: String, val topics: List<String>)
 
+class InMemoryDataStore {
+	// Within the class, I don't box types (such as the user email) for simplicity.
+	private val emailToppingsMap: MutableMap<String, List<String>> = mutableMapOf()
+
+	fun pushUserToppingsSubmission(submission: UserToppingsSubmission) {
+		emailToppingsMap[submission.email] = submission.topics
+	}
+
+	fun showToppingsMap(): MutableMap<String, List<String>> {
+		return emailToppingsMap
+	}
+
+	fun getToppingsCountMap(): List<String> {
+		return emailToppingsMap
+			.flatMap { it.value }
+	}
+}
+
+////////////////////////////////////////////////
+// demo code below this line
 data class Greeting(val id: Long, val content: String)
 
 @RestController
@@ -28,6 +47,11 @@ class GreetingController {
 class ChallengeApplication
 
 fun main(args: Array<String>) {
-	runApplication<ChallengeApplication>(*args)
+	//runApplication<ChallengeApplication>(*args)
 	//print(PersonToppingsChoice("Bob", listOf("cheese", "pepperoni")))
+	val dataStore = InMemoryDataStore()
+	dataStore.pushUserToppingsSubmission(UserToppingsSubmission("foo@com.com", listOf("a", "b", "c")))
+	dataStore.pushUserToppingsSubmission(UserToppingsSubmission("bar@com.com", listOf("a", "c")))
+	dataStore.pushUserToppingsSubmission(UserToppingsSubmission("baz@com.com", listOf("c")))
+	println(dataStore.showToppingsMap())
 }
